9dbec6bbb6cc4a7ad3c25aae414493430b3e0c20
Lukasz Sz <l.szczypin@example.com>
2024-12-02 22:37:53 +0100

    add day1 solution2

--- src/main/java/com/lukaszszszsz/day1/Day1.java
+++ src/main/java/com/lukaszszszsz/day1/Day1.java
@@ -6,19 +6,20 @@ import org.javatuples.Pair;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Day1 {
 
-    public static Integer FIRST = 1;
-    public static Integer SECOND = 2;
+    public static Integer FIRST_ELEM = 0;
+    public static Integer SECOND_ELEM = 1;
 
     public static void main(String[] args) {
         String filename = "day1.txt";
         System.out.println(getSumOfMinDistancesBetweenGivenUnsortedList(filename));
+        System.out.println(getSimilarityScore(filename));
     }
 
 
@@ -38,28 +39,54 @@ public class Day1 {
         return  Math.abs(pair.getValue0()-pair.getValue1());
     }
 
+    
+
     static Collector<Pair<Integer,Integer>,?,Map<Integer, List<Integer>>>collectStreams(){
         return Collectors.teeing(
                 Collectors.mapping(Pair::getValue0, Collectors.toList()),
                 Collectors.mapping(Pair::getValue1, Collectors.toList()),
                 (list1,list2) ->
                 {Map<Integer, List<Integer>> map = new HashMap<>();
-                    map.put(FIRST,list1.stream().sorted().toList());
-                    map.put(SECOND,list2.stream().sorted().toList());
+                    map.put(FIRST_ELEM,list1.stream().sorted().toList());
+                    map.put(SECOND_ELEM,list2.stream().sorted().toList());
                     return map;
                 });
 
     }
 
+    
      static Integer getSumOfMinDistancesBetweenGivenUnsortedList(String filename){
         FileReader fileReader = new FileReader();
-        var collectedLists = fileReader.readFile(filename).map(Day1::lineToTuple).collect(Day1.collectStreams());
-        assert collectedLists.get(FIRST).size() == collectedLists.get(SECOND).size();
-        return IntStream.range(0,collectedLists.get(FIRST).size())
-                .mapToObj(i -> Pair.with(collectedLists.get(FIRST).get(i),collectedLists.get(SECOND).get(i)))
+        
+        List<Integer> firstList = fileReader.readFile(filename).map(Day1::lineToTuple).map(getElem(FIRST_ELEM)).sorted().toList();
+        List<Integer>  secondList = fileReader.readFile(filename).map(Day1::lineToTuple).map(getElem(SECOND_ELEM)).sorted().toList();
+        assert firstList.size() == secondList.size();
+        return IntStream.range(0,firstList.size())
+                .mapToObj(i -> Pair.with(firstList.get(i),secondList.get(i)))
                 .mapToInt(Day1::distance)
                 .sum();
 
+    }
+
+
+    static Collector<Integer,?,Map<Integer,Integer>> getOccurenceMap()
+    {
+        return Collectors.toMap(elem -> elem, elem -> 1, Integer::sum);
+    }
+
+
+    static Integer getSimilarityScore(String filename){
+        FileReader fileReader = new FileReader();
+
+        Map<Integer,Integer>  occurenceMap = fileReader.readFile(filename).map(Day1::lineToTuple).map(getElem(SECOND_ELEM)).collect(getOccurenceMap());
+        return fileReader.readFile(filename).map(Day1::lineToTuple).map(getElem(FIRST_ELEM)).sorted()
+                .map(elem -> occurenceMap.getOrDefault(elem,0)*elem)
+                .mapToInt(Integer::intValue)
+                .sum();
+
+    }
+    private static Function<Pair<Integer, Integer>, Integer> getElem(Integer pos) {
+        return pair -> (Integer) pair.getValue(pos);
     }
 
 }
--- src/test/java/com/lukaszszszsz/day1/Day1Test.java
+++ src/test/java/com/lukaszszszsz/day1/Day1Test.java
@@ -1,6 +1,5 @@
 package com.lukaszszszsz.day1;
 
-import com.lukaszszszsz.commons.FileReader;
 import org.javatuples.Pair;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -8,8 +7,8 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static com.lukaszszszsz.day1.Day1.FIRST;
-import static com.lukaszszszsz.day1.Day1.SECOND;
+import static com.lukaszszszsz.day1.Day1.FIRST_ELEM;
+import static com.lukaszszszsz.day1.Day1.SECOND_ELEM;
 
 public class Day1Test {
 
@@ -29,9 +28,17 @@ public class Day1Test {
     @Test
     public void collectStreamsTest(){
         var res = Stream.of(Pair.with(1,2),Pair.with(1,3)).collect(Day1.collectStreams());
-        Assertions.assertTrue(res.containsKey(FIRST));
-        Assertions.assertTrue(res.containsKey(SECOND));
-        Assertions.assertTrue(res.get(FIRST).containsAll(List.of(1,1)));
+        Assertions.assertTrue(res.containsKey(FIRST_ELEM));
+        Assertions.assertTrue(res.containsKey(SECOND_ELEM));
+        Assertions.assertTrue(res.get(FIRST_ELEM).containsAll(List.of(1,1)));
+
+    }
+    @Test
+    public void getOccurenceMapTest(){
+        var res = Stream.of(4,3,5,3,9,3).collect(Day1.getOccurenceMap());
+        Assertions.assertTrue(res.containsKey(4));
+        Assertions.assertEquals(1,res.get(4));
+        Assertions.assertEquals(3,res.get(3));
 
     }
 
@@ -40,6 +47,13 @@ public class Day1Test {
         String filename = "day1.txt";
         var res = Day1.getSumOfMinDistancesBetweenGivenUnsortedList(filename);
         Assertions.assertEquals(11,res);
+
+    }
+    @Test
+    public void getSimilarityScoreTest(){
+        String filename = "day1.txt";
+        var res = Day1.getSimilarityScore(filename);
+        Assertions.assertEquals(31,res);
 
     }
 }
